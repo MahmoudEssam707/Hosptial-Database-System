4e052bfbe98e820bbc484c9a1e9634ad358ec6bf
Mahmoud Essam <108689968+MahmoudEssam707@users.noreply.github.com>
2023-12-10 19:06:07 +0200

    Some of GUI

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,26 +1,87 @@
-import customtkinter
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
 
 
-class App(customtkinter.CTk):
+class LoginSystem(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title("Login System")
+        self.geometry("300x200")  # Set initial geometry for the login frame
 
-        self.title("my app")
-        self.geometry("400x150")
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.login_frame = tk.Frame(self)
+        self.welcome_frame = tk.Frame(self)
 
-        self.button = customtkinter.CTkButton(
-            self, text="my button", command=self.button_callback
-        )
-        self.button.grid(row=0, column=0, padx=20, pady=20, sticky='ew', columnspan=2)
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+        self.create_login_widgets()
+        self.create_welcome_widgets()
 
-    def button_callback(self):
-        print("button pressed")
+        self.show_login_frame()
+
+    def create_login_widgets(self):
+        label_username = tk.Label(self.login_frame, text="Username:")
+        label_password = tk.Label(self.login_frame, text="Password:")
+        self.entry_username = tk.Entry(self.login_frame)
+        self.entry_password = tk.Entry(self.login_frame, show="*")
+        button_login = tk.Button(self.login_frame, text="Login", command=self.login_clicked)
+
+        label_username.grid(row=0, column=0, padx=10, pady=10)
+        self.entry_username.grid(row=0, column=1, padx=10, pady=10)
+        label_password.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_password.grid(row=1, column=1, padx=10, pady=10)
+        button_login.grid(row=2, column=0, columnspan=2, pady=10)
+
+    def create_welcome_widgets(self):
+        self.welcome_label = tk.Label(self.welcome_frame, text="")
+        button_sign_out = tk.Button(self.welcome_frame, text="Sign Out", command=self.sign_out_clicked)
+
+        self.welcome_label.grid(row=0, column=0, padx=10, pady=10)
+        button_sign_out.grid(row=1, column=0, padx=10, pady=10)
+
+    def authenticate_user(self, username, password):
+        try:
+            # Connect to the database
+            connection = sqlite3.connect("hospital.db")
+            cursor = connection.cursor()
+
+            # Execute SQL query to check user credentials
+            cursor.execute(
+                f"SELECT * FROM Auth WHERE login='{username}' AND password='{password}'"
+            )
+            user = cursor.fetchone()
+
+            # Close the database connection
+            connection.close()
+
+            return user is not None
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+
+    def login_clicked(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        if self.authenticate_user(username, password):
+            self.show_welcome_frame(username)
+        else:
+            messagebox.showerror("Login Error", "You have written something wrong")
+
+    def sign_out_clicked(self):
+        self.show_login_frame()
+
+    def show_login_frame(self):
+        self.login_frame.pack()
+        self.welcome_frame.pack_forget()
+        self.geometry("300x200")
+
+    def show_welcome_frame(self, username):
+        self.login_frame.pack_forget()
+        self.welcome_label.config(text=f"Welcome, {username}!")
+        self.welcome_frame.pack()
+        self.geometry("800x600")
 
 
-app = App()
-app.mainloop()
+if __name__ == "__main__":
+    login_system = LoginSystem()
+    login_system.mainloop()
